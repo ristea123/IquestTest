ec7249c7c304a565e5d01af3d7fa309dd72442ac
Ristea Stefan <stefanristea1@yahoo.com>
2019-11-21 09:24:17 +0200

    Implemented test problems

---
+++ 1.cs
@@ -0,0 +1,16 @@
+﻿//1.	Sa transform literele mici in litere mari intr un string.
+
+static string makeUpperCase(string myStr)
+{
+	string newString = "";
+	int asciiDiff = 'a' - 'A';
+	foreach (var character in myStr)
+	{
+		char newCharacter = character;
+		if (newCharacter >= 'a' && newCharacter <= 'z')
+			newCharacter = newCharacter - AsciiDiff;//Aici e eroarea
+		newString += newCharacter;
+	}
+	return newString;
+}
+
---
+++ 2.cs
@@ -0,0 +1,24 @@
+﻿/*
+Sa verific cate litere au aceeasi pozitie ca in alphabet intr un cuvant(un string care are DOAR litere, mici sau mari)
+Exemple 
+abcd – 4
+abdcd – 2 (a,b)
+Abbc – 2(A,b)
+cBCd – 3(B,C,d)
+*/
+
+static int Solve(string word)
+{
+	int count = 0;
+	for (int i = 0; i < word.Length; i++)
+	{
+		int alphaPoz;
+		if (word[i] >= 'A' && word[i] <= 'Z')
+			alphaPoz = word[i] - 'A';
+		else
+			alphaPoz = word[i] - 'a';
+		if (i == alphaPoz)
+			count++;
+	}
+	return count;
+}
